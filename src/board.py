37d16d15196103dc502dcabce0c53b1acf1d271e
Gabriel Guimaraes <16GuimG11@fullbrook.surrey.sch.uk.com>
2023-06-02 12:52:20 +0100

    Move generation for bishop

--- src/board.py
+++ src/board.py
@@ -92,11 +92,33 @@ class Board:
                     if not self.board[row + offset[0]][col + offset[1]].hasPiece() or self.board[row + offset[0]][col + offset[1]].piece.colour != piece.colour:
                             moves.append((row + offset[0], col + offset[1]))
                     
-        
-
-
         def _bishopMoves():
-            pass
+            # Move offsets
+            offsets = [(direction, -1), (direction, 1), (-direction, -1), (-direction, 1)]
+
+            # For each offset, check if the move is on the board
+            for offset in offsets:
+                for i in range(1, ROWS):
+                    # Calculate new row and col
+                    newRow = row + offset[0] * i
+                    newCol = col + offset[1] * i
+
+                    # If the move is not on the board, break
+                    if newRow not in range(ROWS) or newCol not in range(COLS):
+                        break
+
+                    # If there is a piece of the same colour, break
+                    if self.board[newRow][newCol].hasPiece() and self.board[newRow][newCol].piece.colour == piece.colour:
+                        break
+
+                    # If there is a piece of the opposite colour, add the move and break
+                    if self.board[newRow][newCol].hasPiece() and self.board[newRow][newCol].piece.colour != piece.colour:
+                        moves.append((newRow, newCol))
+                        break
+
+                    # If there is no piece, add the move
+                    if not self.board[newRow][newCol].hasPiece():
+                        moves.append((newRow, newCol))
 
         def _rookMoves():
             pass
