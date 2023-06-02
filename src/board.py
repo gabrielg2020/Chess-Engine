53679de43d2403c3b4e3d17194e58e72d1400971
Gabriel Guimaraes <16GuimG11@fullbrook.surrey.sch.uk.com>
2023-06-02 12:59:19 +0100

    Move generation for rook + less repeated code W

--- src/board.py
+++ src/board.py
@@ -46,6 +46,31 @@ class Board:
         row = square.row
         col = square.col
 
+        def _generateSlidingMoves(offsets): # This function will only be called by: _rookMoves, _bishopMoves, _queenMoves
+            # For each offset, check if the move is on the board
+            for offset in offsets:
+                for i in range(1, ROWS):
+                    # Calculate new row and col
+                    newRow = row + offset[0] * i
+                    newCol = col + offset[1] * i
+
+                    # If the move is not on the board, break
+                    if newRow not in range(ROWS) or newCol not in range(COLS):
+                        break
+
+                    # If there is a piece of the same colour, break
+                    if self.board[newRow][newCol].hasPiece() and self.board[newRow][newCol].piece.colour == piece.colour:
+                        break
+
+                    # If there is a piece of the opposite colour, add the move and break
+                    if self.board[newRow][newCol].hasPiece() and self.board[newRow][newCol].piece.colour != piece.colour:
+                        moves.append((newRow, newCol))
+                        break
+
+                    # If there is no piece, add the move
+                    if not self.board[newRow][newCol].hasPiece():
+                        moves.append((newRow, newCol))
+
         def _pawnMoves():
             # -- 1 Move Foward and 2 Moves Forward --
             # Make sure the move is on the board
@@ -95,33 +120,13 @@ class Board:
         def _bishopMoves():
             # Move offsets
             offsets = [(direction, -1), (direction, 1), (-direction, -1), (-direction, 1)]
-
-            # For each offset, check if the move is on the board
-            for offset in offsets:
-                for i in range(1, ROWS):
-                    # Calculate new row and col
-                    newRow = row + offset[0] * i
-                    newCol = col + offset[1] * i
-
-                    # If the move is not on the board, break
-                    if newRow not in range(ROWS) or newCol not in range(COLS):
-                        break
-
-                    # If there is a piece of the same colour, break
-                    if self.board[newRow][newCol].hasPiece() and self.board[newRow][newCol].piece.colour == piece.colour:
-                        break
-
-                    # If there is a piece of the opposite colour, add the move and break
-                    if self.board[newRow][newCol].hasPiece() and self.board[newRow][newCol].piece.colour != piece.colour:
-                        moves.append((newRow, newCol))
-                        break
-
-                    # If there is no piece, add the move
-                    if not self.board[newRow][newCol].hasPiece():
-                        moves.append((newRow, newCol))
+            _generateSlidingMoves(offsets)
+            
 
         def _rookMoves():
-            pass
+            # Move offsets
+            offsets = [(direction, 0), (-direction, 0), (0, -1), (0, 1)]
+            _generateSlidingMoves(offsets)
 
         def _queenMoves():
             pass
